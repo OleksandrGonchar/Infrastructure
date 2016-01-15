0bf8f555014e96d5f8d0aa6a48bdff30dc57b93e
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-01-15 16:53:39 +0300

    add default controller

--- public/javascripts/services/userData.js
+++ public/javascripts/services/userData.js
@@ -1,25 +1,4 @@
 (function() {
 	"use strict";
 
-	angular
-		.module("app")
-		.factory("userData", userData);
-
-	function userData() {
-
-		var data = { 
-			name: "Vitaliy", 
-			city: "Kyiv" 
-		};
-
-		return {
-			data: data,
-			getCity: getCity
-		};
-
-		function getCity(name) {
-			return name == "Vitaliy" ? "Kiev" : "Unknown";
-		}
-	}
-
 })();
